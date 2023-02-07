3cb85362679efcf925d91afb21c8c310822295fd
jerep <m pip install --upgrade pip>
2023-02-07 20:47:19 +0200

    -

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def main():
     files_to_convert = get_filenames()
     images_dict = convert_to_jpg(files_to_convert)
     processed_images = image_processing(images_dict)
-    merged_financial_statement = merge_images(processed_images)
+    #merged_financial_statement = merge_images(processed_images)
     #ocr_result_to_txt()
 
 def get_filenames():
@@ -24,6 +24,8 @@ def get_filenames():
         files = []
         dir = os.path.join('Financial statements', directory)
         if os.path.isdir(dir):
+            global current_team
+            current_team = directory
             for filename in os.listdir(dir):
                 file = os.path.join(dir, filename)
                 if os.path.isfile(file):
@@ -42,6 +44,8 @@ def convert_to_jpg(pdf_dict):
                 path = f'Financial statements/{team}/{file}'
                 image = convert_from_path(path)
                 images_list.append(image)
+                #print(file)
+                #break
                 # i += 1
                 # if i == 2:
                 #     break
@@ -57,8 +61,8 @@ def image_processing(images_dict):
             #print(images, len(images))
             processed_images_list = []
             for index, image in enumerate(img):
-                if index <= 5:
-                    continue
+                # if index <= 5:
+                #     continue
                 kernel = np.ones((1, 1), np.uint8)
                 image = np.array(image)
                 image = cv2.dilate(image, kernel, iterations=1)
@@ -68,9 +72,12 @@ def image_processing(images_dict):
                 # test_image = cv2.medianBlur(test_image, 3)
                 image = cv2.GaussianBlur(image, (3, 3), 0)
                 processed_images_list.append(image)
+                ocr_result_to_txt(image)
             processed_images[team] = np.vstack(processed_images_list)
+            #break
+            get_correct_dates(starting_seasons[current_team])
             #print(len(processed_images_list))
-            merge_images(processed_images)
+            #merge_images(processed_images)
     return processed_images
 
 def create_dir_for_images(image, team, f_statement_season):
@@ -95,9 +102,9 @@ def save_files(image, team, f_statement_season):
         os.chdir(f'{os.getcwd()}\{team}')
         image.save(f'{team} financial statement {f_statement_season}.jpg', 'JPEG')
         os.chdir('..')
-    get_correct_dates(team, f_statement_season)
+    #get_correct_dates(team, f_statement_season)
 
-def get_correct_dates(team, date_str):
+def get_correct_dates(date_str):
     start_year, end_year = [int(x) for x in date_str.split("-")]
     #start_date = datetime(start_year, 1, 1)
     end_date = datetime(end_year, 12, 31)
@@ -106,7 +113,7 @@ def get_correct_dates(team, date_str):
     new_end_date = end_date + timedelta(days=365)
 
     new_date_str = new_start_date.strftime("%Y") + "-" + new_end_date.strftime("%Y")
-    starting_seasons[team] = new_date_str
+    starting_seasons[current_team] = new_date_str
 
 
 
@@ -117,41 +124,70 @@ def merge_images(images):
         #print(merged_image)
         create_dir_for_images(merged_image, team, f_statement_season)
 
-def ocr_result_to_txt():
-    dir_path = f'{os.getcwd()}\Processed images'
-    os.chdir(dir_path)
-    f = None
-    for team in os.listdir(dir_path):
-        dir_path = os.path.join(dir_path, team)
-        os.chdir(dir_path)
-        for filename in os.listdir(dir_path):
-            print(os.getcwd())
-            season = filename[len(filename) - 13: len(filename) - 4]
-            image = Image.open(filename)
-            image_text = pytesseract.image_to_string(image)
-            #print(os.getcwd())
-            create_dir_for_txt(team, season)
-            #print(image_text)
-            #f = open(f"\{team}\{season}.txt", "w", encoding="utf-8")
-            #f.write(image.text)
-            os.chdir(dir_path)
-    #f.close()
+def ocr_result_to_txt(img):
+    #print(os.getcwd(), current_team)
+    #print(starting_seasons[current_team], os.getcwd())
+    #print(os.path.basename(os.getcwd()))
+    # if os.path.basename(os.getcwd()) == 'Financial statements in txt':
+    #     try:
+    #         os.mkdir(f'{os.getcwd()}/{current_team}')
+    #         os.chdir(f'{os.getcwd()}/{current_team}')
+    #     except FileExistsError:
+    #         pass
+    #print(os.getcwd())
+    if os.path.basename(os.getcwd()) == current_team:
+        f = open(f'{current_team} {starting_seasons[current_team]}.txt', 'a', encoding='utf-8')
+        f.write(pytesseract.image_to_string(img))
+        f.close()
+    create_dir_for_txt()
+    #os.chdir(f'{os.getcwd()}\Financial Statements in txt')
+
+# def ocr_result_to_txt(img):
+#     pass
+
+
+# def ocr_result_to_txt(img):
+#     dir_path = f'{os.getcwd()}\Processed images'
+#     os.chdir(dir_path)
+#     f = None
+#     for team in os.listdir(dir_path):
+#         dir_path = os.path.join(dir_path, team)
+#         os.chdir(dir_path)
+#         for filename in os.listdir(dir_path):
+#             print(os.getcwd())
+#             season = filename[len(filename) - 13: len(filename) - 4]
+#             image = Image.open(filename)
+#             image_text = pytesseract.image_to_string(image)
+#             #print(os.getcwd())
+#             create_dir_for_txt(team, season)
+#             #print(image_text)
+#             #f = open(f"\{team}\{season}.txt", "w", encoding="utf-8")
+#             #f.write(image.text)
+#             os.chdir(dir_path)
+#     #f.close()
 
     #f = open("Financial statements in txt/Forest Green Rovers/Season1011.txt", "a", encoding="utf-8")
     # for img in images:
     #     f.write(pytesseract.image_to_string(img))
     #f.close()
 
-def create_dir_for_txt(team, season):
+def create_dir_for_txt():
     #print(os.path.basename(os.getcwd()), os.getcwd())
+    #print(starting_seasons[current_team], os.getcwd())
     try:
-        if os.path.basename(os.getcwd()) == team:
-            os.chdir('../..')
-        os.mkdir(f'{os.getcwd()}/Financial statements in txt')
-        os.chdir(f'{os.getcwd()}/Financial statements in txt')
-        #print(os.getcwd())
+        if os.path.basename(os.getcwd()) == current_team:
+            pass
+        elif os.path.basename(os.getcwd()) == 'Financial statements in txt':
+            try:
+                os.mkdir(f'{os.getcwd()}/{current_team}')
+                os.chdir(f'{os.getcwd()}/{current_team}')
+            except FileExistsError:
+                os.chdir(f'{os.getcwd()}/{current_team}')
+        else:
+            os.mkdir(f'{os.getcwd()}/Financial statements in txt')
+            os.chdir(f'{os.getcwd()}/Financial statements in txt')
     except FileExistsError:
-        os.chdir(f'{os.getcwd()}/Financial statements in txt')
+        os.chdir(f'{os.getcwd()}/Financial statements in txt') #changing and making directories, depending on where the user is
 
 
 main()
