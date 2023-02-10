730a7a8ed7c61d4ef79088487de8b352cb0c411f
jerep <m pip install --upgrade pip>
2023-02-10 19:22:51 +0200

    Continuing the function

--- extracting_data.py
+++ extracting_data.py
@@ -3,6 +3,32 @@ import os
 #from main import main
 import file_handling
 
+balance_sheet_dict = {
+    "Fixed assets": None,
+    "Intangible assets": None,
+    "Tangible assets": None,
+    "Current assets": None,
+    "Stocks": None,
+    "Debtors: due within one year": None,
+    "Debtors: due after more than one year": None,
+    "Cash at bank and in hand": None,
+    "Creditors: amounts falling due within one year": None,
+    "Net current liabilities": None,
+    "Total assets less current liabilities": None,
+    "Creditors: amounts falling due after more than one year": None,
+    "Net liabilities": None,
+    "Capital and reserves": None,
+    "Called up share capital": None,
+    "Share premium account": None,
+    "Unrealised profit reserve": None,
+    "Profit and loss account": None,
+    "Shareholders deficit": None
+}
+
+
+
+
+
 # def main():
 #     reg_exp_extraction()
 
@@ -24,10 +50,11 @@ def reg_exp_extraction():
 def file_reading(file, season, team):
     this_year = season.split('-')[1]
     last_year = season.split('-')[0]
+    file = file.readlines()
     #print(latest_year, former_year)
-    keywords = ['turnover', 'revenue', 'cost of sales', 'gross profit', 'administrative expenses', 'other operating income']
+    keywords = ['turnover', 'revenue', 'cost of sales', 'gross profit', 'administrative expenses', 'other operating income', 'profit/(loss)', 'operating profit']
     data = {}
-    for line in file:
+    for index, line in enumerate(file):
         line = line.strip()
         line = line.lower()
         line = line.split()
@@ -44,11 +71,16 @@ def file_reading(file, season, team):
                 add_to_dict(data, team, this_year, last_year, string_with_spaces1, this_year_value, last_year_value)
             elif line[0] in keywords:
                 add_to_dict(data, team, this_year, last_year, line[0], this_year_value, last_year_value)
+            elif line[2] == this_year[2:] and file[index + 1][1] == '£':
+                pass
+                #print(file[index], file[index + 1])
+                #print(line[index + 1])
+                #print(line[2])
         except IndexError:
             pass
         #print(line)
     print(data)
-    file.close()
+    #file.close()
 
 def determine_values(line):
     this_year_value = line[len(line) - 2]
