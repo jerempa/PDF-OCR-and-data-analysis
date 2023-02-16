8d15180ac671f9744681e2ae6894112323f7ba29
jerep <m pip install --upgrade pip>
2023-02-16 17:34:54 +0200

    Parsing supports every needed league

--- fetch_data_from_transfermarkt.py
+++ fetch_data_from_transfermarkt.py
@@ -10,6 +10,8 @@ base_url = 'https://www.transfermarkt.com'
 headers = {'User-Agent':
                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'}
 
+teams = ['Brighton & Hove Albion', 'Leeds United', 'Blackpool FC' 'Huddersfield Town', 'Hull City', 'Queens Park Rangers', 'Ipswich Town']
+
 def get_request():
     url_bpl = 'https://www.transfermarkt.com/quickselect/teams/GB1'
     url_champ = 'https://www.transfermarkt.com/quickselect/teams/GB2'
@@ -28,12 +30,19 @@ def get_request():
 
 
     response_bpl = requests.get(url_bpl, headers=headers)
-    # response_champ = requests.get(url_champ, headers=headers)
-    # response_l1 = requests.get(url_l1, headers=headers)
-    # response_l2 = requests.get(url_l2, headers=headers)
+    response_champ = requests.get(url_champ, headers=headers)
+    response_l1 = requests.get(url_l1, headers=headers)
+    response_l2 = requests.get(url_l2, headers=headers)
 
-    teams_dict = parse_league_and_position(response_bpl.json())
-    create_df_from_dict(teams_dict)
+    test = [response_bpl, response_champ, response_l1, response_l2]
+    test1 = []
+
+    for res in test:
+        teams_dict = parse_league_and_position(res.json())
+        test1.append(teams_dict)
+    for dic in test1:
+        if dic:
+            print(create_df_from_dict(dic))
 
 
     #print(response_bpl.status_code, response_bpl, response_bpl.text)
@@ -42,23 +51,13 @@ def get_request():
     # print(response_l2.status_code, response_l2, response_l2.text)
 
 def parse_league_and_position(res):
-    print(type(res), res)
+    #print(type(res), res)
     #print(headers)
     text = 'platzierungen'
     teams_dict = {}
     for data in res:
-        season_list = []
-        league_tier_list = []
-        league_list = []
-        win_list = []
-        draw_list = []
-        lost_list = []
-        goals_and_conceded_goals_list = []
-        gd_list = []
-        rank_list = []
-        manager_list = []
         team = None
-        if data['name'] == 'Brighton & Hove Albion' or data['name'] == 'Leeds United':
+        if data['name'] in teams:
             team = data['name']
             url = f'{base_url}{data["link"]}'
             params = url.split('/')
@@ -68,47 +67,39 @@ def parse_league_and_position(res):
             pageSoup = BeautifulSoup(test_res.content, 'html.parser')
             table = pageSoup.find('table', class_='items')
             rows = table.find_all('tr')[1:]
+            team_data = {
+                'Season': [],
+                'League': [],
+                'League level': [],
+                'Wins': [],
+                'Draws': [],
+                'Losses': [],
+                'Goals': [],
+                'Goal difference': [],
+                'Rank': [],
+                'Manager': []
+            }
             #seasons = pageSoup.find_all('td', {'class': 'zentriert'})
             for season in rows:
                 cols = season.find_all('td')
                 data = [td.text for td in cols]
+                season = data[0]
                 try:
-                    season = data[0]
-                    season_list.append(season)
-
-                    league = data[2]
-                    league_list.append(league)
-
-                    league_tier = data[3]
-                    league_tier_list.append(league_tier)
-
-                    wins = data[4]
-                    win_list.append(wins)
-
-                    draws = data[5]
-                    draw_list.append(draws)
-
-                    losses = data[6]
-                    lost_list.append(losses)
-
-                    scored_conceded = data[7]
-                    goals_and_conceded_goals_list.append(scored_conceded)
-
-                    gd = data[8]
-                    gd_list.append(gd)
-
-                    rank = data[10]
-                    rank_list.append(rank)
-
-                    manager = data[11]
-                    manager_list.append(manager)
+                    team_data['Season'].append(data[0])
+                    team_data['League'].append(data[2])
+                    team_data['League level'].append(data[3])
+                    team_data['Wins'].append(data[4])
+                    team_data['Draws'].append(data[5])
+                    team_data['Losses'].append(data[6])
+                    team_data['Goals'].append(data[7])
+                    team_data['Goal difference'].append(data[8])
+                    team_data['Rank'].append(data[10])
+                    team_data['Manager'].append(data[11])
                 except IndexError:
                     pass
                 if season[0] == '9' or season[0] == '8':
                     break
-            teams_dict[team] = {'Season': season_list, 'League': league_list, 'League level': league_tier_list, 'Wins': win_list,
-                                'Draws': draw_list, 'Losses': lost_list, 'Goals': goals_and_conceded_goals_list,
-                                'Goal difference': gd_list, 'Rank': rank_list, 'Manager': manager_list}
+            teams_dict[team] = team_data
     return teams_dict
 
 def create_df_from_dict(teams_dict):
@@ -118,14 +109,20 @@ def create_df_from_dict(teams_dict):
         team_df['Team'] = team
         team_df = team_df[['Team'] + list(data.keys())]
         team_data.append(team_df)
-
+    #print(team_data)
+    # try:
     df = pd.concat(team_data).reset_index(drop=True)
+
+    # except ValueError:
+    #     pass
+
+    return df
 
     # pd.set_option('display.max_columns', None)
     # pd.set_option('display.max_rows', None)
-    print(df)
+    #print(df)
 
-    return df
+
 
 # def loop_through_history_data(rows):
 #     season_list = []
