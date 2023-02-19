5e6cb6c6341093be9228d278d6ca6bc68e08fa91
jerep <m pip install --upgrade pip>
2023-02-19 14:20:58 +0200

    Calculating attendance to capacity %

--- data_fetchers/df_operations.py
+++ data_fetchers/df_operations.py
@@ -1,8 +1,14 @@
 import pandas as pd
 from file_operations import file_handling
 
-stadium_capacities = {'Brighton & Hove Albion': '31,800', 'Leeds': '37,890', 'Blackpool FC': '16,220', 'Huddersfield Town': '24,500', 'Hull City': '25,586',
-                      'Queens Park Rangers': '18,360', 'Ipswich Town': '29,673'}
+stadium_capacities = {'Brighton & Hove Albion': [31800, 12, 8850], 'Leeds United': 37890, 'Blackpool FC': 16220, 'Huddersfield Town': 24500, 'Hull City': 25586,
+                      'Queens Park Rangers': 18360, 'Ipswich Town': 29673}
+
+#Hull from 2002
+#Hull before that 15,160
+#Brighton from 2011
+#Brighton before that 8,850
+
 
 
 def print_df():
@@ -37,3 +43,22 @@ def create_df_from_dict(teams_dict):
 
 
     return df
+
+def return_attendance_percentage(team_name, avg_attendance_list):
+    #avg_attendance_list = ['36,461', '34,376', '421', '27,643', '33,598', '31,525', '27,698', '22,446', '24,052', '25,092', '21,572', '23,283', '27,299', '24,818', '23,639', '26,546', '21,613', '22,353', '29,207', '36,666', '39,146', '39,752', '38,974', '39,155']
+    #avg_attendance_list = ["31,487", "30,943", "629", "22,368", "30,426", "30,402", "27,996", "25,583", "25,645", "27,283", "26,236", "20,028", "7,355", "6,467", "6,092", "5,937", "6,048", "6,802", "6,426", "6,248", "6,651", "6,598", None, None]
+    percentages = []
+    for team, capacity in stadium_capacities.items():
+        if team == team_name:
+            for index, attendance in enumerate(avg_attendance_list):
+                if not attendance:
+                    percentages.append(None)
+                    continue
+                attendance = int(attendance.replace(',', ''))
+                if isinstance(capacity, list) and index >= capacity[1]:
+                    percentage = attendance/capacity[2] * 100
+                else:
+                    percentage = attendance/capacity[0] * 100
+                percentages.append(round(percentage, 2))
+
+    return percentages
