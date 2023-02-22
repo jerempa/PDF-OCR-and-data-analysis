9ea97093567778479426b04e4f8d4f8fd5ee824d
jerep <m pip install --upgrade pip>
2023-02-22 17:25:10 +0200

    Avg median for squad value

--- data_visualization_and_analysis/calculations.py
+++ data_visualization_and_analysis/calculations.py
@@ -10,7 +10,8 @@ def main():
         df = values_for_analysis.league_tier_throughout_years(team)
         #print(df)
         #show_average_attendace_to_capacity(df, team)
-        bought_players_avg_and_median(df, team)
+        #bought_players_avg_and_median(df, team)
+        squad_value_avd_median(df, team)
 
 def show_average_attendace_to_capacity(df, team):
     premier_league_attendance = []
@@ -67,6 +68,33 @@ def bought_players_avg_and_median(df, team):
     try:
         print(f'Average Premier League: {round(sum(premier_league_arrivals) / len(premier_league_arrivals), 2)} M€'
               f' Average Championship: {round(sum(championship_arrivals) / len(championship_arrivals), 2)} M€')
+              #f' Average League One: {round(sum(league_one_arrivals) / len(league_one_arrivals), 2)} M€')
+    except ZeroDivisionError:
+        pass
+
+def squad_value_avd_median(df, team):
+    premier_league_squad_value = []
+    championship_squad_value = []
+    league_one_squad_value = []
+    print(f'\n{team}')
+    for index, row in df.iterrows():
+        if row['Year'] > 1990:  # don't take into account covid year without spectators
+            if row['League level'] == 'First Tier':
+                premier_league_squad_value.append(row['Infl adjusted squad market value M€'])
+            elif row['League level'] == 'Second Tier':
+                championship_squad_value.append(row['Infl adjusted squad market value M€'])
+            elif row['League level'] == 'Third Tier':
+                league_one_squad_value.append(row['Infl adjusted squad market value M€'])
+    #print(premier_league_arrivals, championship_arrivals, league_one_arrivals)
+    try:
+        print(f'Median Premier League: {round(statistics.median(premier_league_squad_value), 2)} M€'
+              f' Median Championship: {round(statistics.median(championship_squad_value), 2)} M€')
+              #f' Median League One: {round(statistics.median(league_one_arrivals), 2)} M€')
+    except statistics.StatisticsError:
+        pass
+    try:
+        print(f'Average Premier League: {round(sum(premier_league_squad_value) / len(premier_league_squad_value), 2)} M€'
+              f' Average Championship: {round(sum(championship_squad_value) / len(championship_squad_value), 2)} M€')
               #f' Average League One: {round(sum(league_one_arrivals) / len(league_one_arrivals), 2)} M€')
     except ZeroDivisionError:
         pass
